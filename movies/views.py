e0e1d69a503cd6ea1126cc128970c39afa75a356
박진현 <pjiny1220@naver.com>
2024-11-08 17:37:05 +0900

    first

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_safe
+from .models import Movie, Genre
+from django.http import JsonResponse
+from django.core import serializers
 
 
 # Create your views here.
 @require_safe
 def index(request):
-    pass
-
+    movies = Movie.objects.all()
+    genres = Genre.objects.all()
+    context = {
+        'movies': movies,
+        'genres': genres,
+    }
+    return render(request, 'movies/index.html', context)
 
 def filter_genre(request):
-    pass
+    genre_id = request.GET.get('genre')
+    genre = Genre.objects.get(pk=genre_id)
+    movies = genre.movie_set.all()
+    tmp_list = serializers.serialize('json',movies)
+    print(tmp_list)
+
+    return JsonResponse(tmp_list,safe=False)
+    
 
 
 @require_safe
